891f9f0a3f61048bcee82b518f9e1485797daadc
Alonya Lafayette <alonyalafayette1@gmail.com>
2017-08-21 15:22:21 -0400

    finished project

--- dal.js
+++ dal.js
@@ -2,42 +2,41 @@
 const users = require('./data')
 
 function getUsers () {
-  return users
+  return users.users
 }
 
-function getUser (userID) {
-  let chosenUser = {}
-  for (var i = 0; i < users.length; i++) {
-    if (users[i].id === userId) {
-      chosenUser = users[i]
+function getUser (userId) {
+  let chosenUser = {};
+  for (var i = 0; i < users.users.length; i++) {
+    if (users.users[i].id == userId) {
+      chosenUser = users.users[i]
     }
   }
-  return chosenUser
+  console.log(chosenUser);
+  return chosenUser;
 }
-
-function getUserJob (userJob) {
-  let jobs = {}
-  for (var i = 0; i < users.length; i++) {
-    if (users[i].job === userJob) {
-      jobs = users[i].job
-    }
-  }
-}
-
-function getCompany (userCompany) {
-  let company = {}
-  for (var i = 0; i < users.length; i++) {
-    if (users[i].company === userCompany) {
-      company = users[i].company
-    // }else{
-    //   return "Available for hire"
-    }
-  }
-}
+getUser(3);
+// function getUserJob (userJob) {
+//   let jobs = {}
+//   for (var i = 0; i < users.length; i++) {
+//     if (users[i].job === userJob) {
+//       jobs = users[i].job
+//     }
+//   }
+// }
+//
+// function getCompany (userCompany) {
+//   let company = {}
+//   for (var i = 0; i < users.length; i++) {
+//     if (users[i].company === userCompany) {
+//       company = users[i].company
+//     }
+//   }
+// }
 
 module.exports = {
   getUsers: getUsers,
   getUser: getUser,
-  getUserJob: getUserJob,
-  getCompany: getCompany,
+  // getUserJob: getUserJob,
+  // getCompany: getCompany,
 }
